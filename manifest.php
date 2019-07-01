798bcf5b6a74a861d5b6f02581199f5a74bb8371
yuryfilipovich <yury.filipovich@1pt.com>
2019-07-01 11:17:35 +0300

    TAO-8586 -  delete unnecessary controller, template, translate, change managementRole

--- manifest.php
+++ manifest.php
@@ -1,23 +1,25 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2019 (original work) Open Assessment Technologies SA;
- *               
- * 
+ *
+ *
  */
+
+use oat\taoSync\model\SyncService;
 
 /**
  * Generated using taoDevTools 6.1.0
@@ -30,23 +32,18 @@ return [
     'version' => '0.0.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [],
-    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoSyncServerManager',
+    'managementRole' => SyncService::TAO_SYNC_ROLE,
     'acl' => [
-        ['grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoSyncServerManager', ['ext'=>'taoSyncServer']],
+        ['grant', SyncService::TAO_SYNC_ROLE, ['ext' => 'taoSyncServer']],
     ],
     'install' => [],
     'uninstall' => [],
-    'routes' =>[
-        '/taoSyncServer' => 'oat\\taoSyncServer\\controller'
-    ],
+    'routes' => [],
     'constants' => [
         # views directory
-        "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
-        
+        "DIR_VIEWS" => dirname(__FILE__) . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR,
+
         #BASE URL (usually the domain root)
-        'BASE_URL' => ROOT_URL.'taoSyncServer/',
-    ],
-    'extra' => [
-        'structures' => dirname(__FILE__).DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.'structures.xml',
+        'BASE_URL' => ROOT_URL . 'taoSyncServer/',
     ]
 ];
